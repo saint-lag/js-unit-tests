3214153c9fc371d799334719ac80c24f27f4949a
saint-lag <maiasena.gabriel@gmail.com>
2022-01-17 17:39:35 -0300

    createStudent: PASS

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  return { name: student, feedback: () => 'Eita pessoa boa!' };
+};
 
 module.exports = createStudent;
