96029616c5ee47627b427df3c2d0f39573bd4b5f
saint-lag <maiasena.gabriel@gmail.com>
2022-01-17 18:22:43 -0300

    getCharacter: PASS

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,14 +42,81 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se os parâmetros não são Case Sensitive.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  // ESCREVA SEUS TESTES ABAIXO:
+  it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter() === undefined);
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro "Arya" ', () => {
+    expect(
+      getCharacter('Arya') ===
+        {
+          name: 'Arya Stark',
+          class: 'Rogue',
+          phrases: ['Not today', 'A girl has no name.'],
+        }
+    );
+    expect(
+      getCharacter('ARYA') ===
+        {
+          name: 'Arya Stark',
+          class: 'Rogue',
+          phrases: ['Not today', 'A girl has no name.'],
+        }
+    );
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro "Brienne" ', () => {
+    expect(
+      getCharacter('Brienne') ===
+        {
+          name: 'Brienne Tarth',
+          class: 'Knight',
+          phrases: [
+            'Im No Lady, Your Grace.',
+            'I, Brienne Of Tarth, Sentence You To Die.',
+          ],
+        }
+    );
+    expect(
+      getCharacter('BRIENNE') ===
+        {
+          name: 'Brienne Tarth',
+          class: 'Knight',
+          phrases: [
+            'Im No Lady, Your Grace.',
+            'I, Brienne Of Tarth, Sentence You To Die.',
+          ],
+        }
+    );
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro "Melissandre" ', () => {
+    expect(
+      getCharacter('Melissandre') ===
+        {
+          name: 'Melissandre',
+          class: 'Necromancer',
+          phrases: [
+            'Death By Fire Is The Purest Death.',
+            'For The Night Is Dark And Full Of Terrors.',
+          ],
+        }
+    );
+
+    expect(
+      getCharacter('MELISSANDRE') ===
+        {
+          name: 'Melissandre',
+          class: 'Necromancer',
+          phrases: [
+            'Death By Fire Is The Purest Death.',
+            'For The Night Is Dark And Full Of Terrors.',
+          ],
+        }
+    );
+  });
+  it('Teste se ao passar um nome que não está na tabela, a função retorna undefined.', () => {
+    expect(getCharacter('Pedro') === undefined);
+    expect(getCharacter('Maria') === undefined);
+    expect(getCharacter('') === undefined);
+    expect(getCharacter() === undefined);
   });
 });
