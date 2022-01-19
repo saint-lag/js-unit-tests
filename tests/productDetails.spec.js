4d0047d9ca265af519ae8a4b7d2e28c636712a1e
saint-lag <maiasena.gabriel@gmail.com>
2022-01-18 22:21:31 -0300

    productDetails: PASS

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,14 +27,32 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Teste se productDetails é uma função.', () => {
+    expect(typeof productDetails).toEqual('function');
+  });
+  it('Teste se o retorno da função é um array.', () => {
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
+  });
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+    expect(productDetails('Alcool gel', 'Máscara').length).toBe(2);
+  });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    const productsArray = productDetails('Alcool gel', 'Máscara');
+    for (let item in productsArray) {
+      expect(typeof productsArray[item]).toEqual('object');
+    }
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    const productsArray = productDetails('Alcool gel', 'Máscara');
+    expect(new Set(productsArray).size).toEqual(productsArray.length);
+  });
+  it('Teste se os dois productIds terminam com 123.', () => {
+    const productsArray = productDetails('Alcool gel', 'Máscara');
+    for (let item in productsArray) {
+      const itemChars = productsArray[item].details.productId.split('');
+      expect(itemChars[itemChars.length - 1]).toBe('3');
+      expect(itemChars[itemChars.length - 2]).toBe('2');
+      expect(itemChars[itemChars.length - 3]).toBe('1');
+    }
   });
 });
